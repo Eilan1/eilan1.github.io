8643659ecff0679e7c0e8f251b4b5d2501d5e248
Eilan1 <uralatak2@gmail.com>
2026-05-21 15:20:46 +0300

    Implement XP gain and rendering functions

--- js/features/xp-system.js
+++ js/features/xp-system.js
@@ -1,1 +1,29 @@
+function gainXP(amount){
 
+  APP.xp += amount;
+
+  const nextLevel = APP.level * 100;
+
+  if(APP.xp >= nextLevel){
+    APP.level++;
+    APP.xp = 0;
+  }
+
+  save();
+  renderXP();
+}
+
+function renderXP(){
+
+  const el = document.getElementById("xp-bar");
+
+  if(!el) return;
+
+  el.innerHTML = `
+    <div class="card">
+      LEVEL ${APP.level}
+      <br><br>
+      XP: ${APP.xp}/${APP.level*100}
+    </div>
+  `;
+}
